dcc1c8a9f7828733030923d632115bf6685e8203
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-02-18 19:48:29 +0100

    Growing plant - Initial commit
    Follow camera finished (under testing)
    Basic growing plant 3d model.
    Basic growing mechanic implemented.

---
+++ GrowingPlant/Assets/Scripts/followCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{
+    public Transform tipOfPlant;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x,
+                                                                            tipOfPlant.position.y / 2,
+                                                                            -tipOfPlant.position.y / 2), Time.deltaTime);
+    }
+}
---
+++ GrowingPlant/Assets/Scripts/plant.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class plant : MonoBehaviour
+{
+    public float growingRate = .001f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(transform.localScale.x, 
+                                                                                transform.localScale.y,
+                                                                                transform.localScale.z + growingRate), Time.deltaTime);
+
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(transform.localScale.x + (growingRate * .1f),
+                                                                                transform.localScale.y + (growingRate * .1f),
+                                                                                transform.localScale.z), Time.deltaTime);
+        
+    }
+}
