427d66e7f4c0da70b8e65b20b8c2551bf13c13ab
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-03-04 19:48:59 +0100

    Initial commit. Worked in player movement and camera

---
+++ 3rd person shoot and cover game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public bool lockCursor = true;
+
+    public float mouseSensitivity = 5.0f;
+    public float dstFromTarget = 1.5f;
+    public float cameraSmoothTime = .1f;
+
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    public Transform target;
+
+    private float m_yaw;
+    private float m_pitch;
+
+    private Vector3 m_currentRotation;
+    private Vector3 m_rotationSmoothVelocity;
+
+    //private Camera m_playerCamera;
+
+    void Start()
+    {
+        //m_playerCamera.GetComponent<Camera>();
+        LockCursor(lockCursor);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            lockCursor = !lockCursor;
+            LockCursor(lockCursor);
+        }
+    }
+
+    void LateUpdate()
+    {
+        m_yaw += Input.GetAxis("Mouse X");
+        m_pitch -= Input.GetAxis("Mouse Y");
+        m_pitch = Mathf.Clamp(m_pitch, pitchMinMax.x, pitchMinMax.y);
+
+        m_currentRotation = Vector3.SmoothDamp(m_currentRotation, new Vector3(m_pitch, m_yaw), ref m_rotationSmoothVelocity, cameraSmoothTime);
+
+        transform.eulerAngles = m_currentRotation;
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+    }
+
+    void LockCursor(bool t_lockCursor)
+    {
+        Cursor.lockState = (t_lockCursor) ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !t_lockCursor;
+    }
+}
---
+++ 3rd person shoot and cover game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    public float gravityScale = 5.0f;
+    public float movementSmoothTime = .1f;
+
+    private float m_turnSmoothVelocity;
+
+    private Vector3 m_moveDirection;
+
+    private Animator m_animController;
+    private CharacterController m_cController;
+    private Transform m_cameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animController = GetComponentInChildren<Animator>();
+        m_cController = GetComponent<CharacterController>();
+        m_cameraTransform = GetComponentInChildren<CameraController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_moveDirection = new Vector3(Input.GetAxis("Horizontal") * movementSpeed,      // X axis movement
+            Physics.gravity.y * gravityScale,                                           // Gravity
+            Input.GetAxis("Vertical") * movementSpeed);                                 // Y axis movement
+
+        m_cController.Move(m_moveDirection * Time.deltaTime);       // Apply the movement
+
+        transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y,
+            Mathf.Rad2Deg + m_cameraTransform.eulerAngles.y,
+            ref m_turnSmoothVelocity, movementSmoothTime);
+
+        m_animController.SetFloat("playerSpeed", Mathf.Abs(Input.GetAxisRaw("Horizontal") + Input.GetAxisRaw("Vertical")));
+    }
+}
