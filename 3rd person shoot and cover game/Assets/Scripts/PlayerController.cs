365831e0dd9866522004b35ea2f4435acd69ea2d
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-03-05 22:53:49 +0100

    Working in the player movement

--- 3rd person shoot and cover game/Assets/Scripts/PlayerController.cs
+++ 3rd person shoot and cover game/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed = 5.0f;
+    public float walkingSpeed = 5.0f;
+    public float runningSpeed = 10.0f;
     public float gravityScale = 5.0f;
     public float movementSmoothTime = .1f;
+    public float speedSmoothTime = .1f;
 
+    private bool m_isRunning;
+
+    private float m_currSpeed;
     private float m_turnSmoothVelocity;
+    private float m_speedSmoothVelocity;
 
     private Vector3 m_moveDirection;
 
@@ -20,23 +26,27 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         m_animController = GetComponentInChildren<Animator>();
-        m_cController = GetComponent<CharacterController>();
         m_cameraTransform = GetComponentInChildren<CameraController>().transform;
+        m_cController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_moveDirection = new Vector3(Input.GetAxis("Horizontal") * movementSpeed,      // X axis movement
-            Physics.gravity.y * gravityScale,                                           // Gravity
-            Input.GetAxis("Vertical") * movementSpeed);                                 // Y axis movement
+        m_isRunning = Input.GetKey(KeyCode.LeftShift);
+        float targetSpeed = ((m_isRunning) ? runningSpeed : walkingSpeed);
+        m_currSpeed = Mathf.SmoothDamp(m_currSpeed, targetSpeed, ref m_speedSmoothVelocity, speedSmoothTime);
+
+        m_moveDirection = new Vector3(Input.GetAxis("Horizontal") * m_currSpeed,      // X axis movement
+            Physics.gravity.y * gravityScale,                                         // Gravity
+            Input.GetAxis("Vertical") * m_currSpeed);                                 // Y axis movement
 
         m_cController.Move(m_moveDirection * Time.deltaTime);       // Apply the movement
 
         transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y,
-            Mathf.Rad2Deg + m_cameraTransform.eulerAngles.y,
+            m_cameraTransform.eulerAngles.y,
             ref m_turnSmoothVelocity, movementSmoothTime);
 
-        m_animController.SetFloat("playerSpeed", Mathf.Abs(Input.GetAxisRaw("Horizontal") + Input.GetAxisRaw("Vertical")));
+        m_animController.SetFloat("playerSpeed", Input.GetAxis("Horizontal") + Input.GetAxis("Vertical"));
     }
 }
