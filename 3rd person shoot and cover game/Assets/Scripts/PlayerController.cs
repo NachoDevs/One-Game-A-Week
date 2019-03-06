29159f17ce767b0bf1c9844f37b13f40c3042ad1
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-03-06 12:09:39 +0100

    Movement tweaked and ready for testing.

--- 3rd person shoot and cover game/Assets/Scripts/PlayerController.cs
+++ 3rd person shoot and cover game/Assets/Scripts/PlayerController.cs
@@ -9,43 +9,53 @@ public class PlayerController : MonoBehaviour
     public float gravityScale = 5.0f;
     public float movementSmoothTime = .1f;
     public float speedSmoothTime = .1f;
+    public float turnSmoothTime = .1f;
 
     private bool m_isRunning;
 
     private float m_currSpeed;
-    private float m_turnSmoothVelocity;
     private float m_speedSmoothVelocity;
+    private float m_turnSmoothVelocity;
+    private float m_targetRotation;
+    private float m_targetSpeed;
+
+    private Vector2 m_input;
+    private Vector2 m_inputDir;
 
     private Vector3 m_moveDirection;
 
     private Animator m_animController;
-    private CharacterController m_cController;
+    //private CharacterController m_cController;
     private Transform m_cameraTransform;
+    private Rigidbody m_rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         m_animController = GetComponentInChildren<Animator>();
         m_cameraTransform = GetComponentInChildren<CameraController>().transform;
-        m_cController = GetComponent<CharacterController>();
+        //m_cController = GetComponent<CharacterController>();
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        m_input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        m_inputDir = m_input.normalized;
+
+        m_targetRotation = Mathf.Atan2(m_inputDir.x, m_inputDir.y) * Mathf.Rad2Deg + m_cameraTransform.eulerAngles.y;
+        transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, m_targetRotation, ref m_turnSmoothVelocity, turnSmoothTime);
+
         m_isRunning = Input.GetKey(KeyCode.LeftShift);
-        float targetSpeed = ((m_isRunning) ? runningSpeed : walkingSpeed);
-        m_currSpeed = Mathf.SmoothDamp(m_currSpeed, targetSpeed, ref m_speedSmoothVelocity, speedSmoothTime);
+        m_targetSpeed = ((m_isRunning) ? runningSpeed : walkingSpeed) * m_inputDir.magnitude;
+        m_currSpeed = Mathf.SmoothDamp(m_currSpeed, m_targetSpeed, ref m_speedSmoothVelocity, speedSmoothTime);
 
-        m_moveDirection = new Vector3(Input.GetAxis("Horizontal") * m_currSpeed,      // X axis movement
-            Physics.gravity.y * gravityScale,                                         // Gravity
-            Input.GetAxis("Vertical") * m_currSpeed);                                 // Y axis movement
-
-        m_cController.Move(m_moveDirection * Time.deltaTime);       // Apply the movement
-
-        transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y,
-            m_cameraTransform.eulerAngles.y,
-            ref m_turnSmoothVelocity, movementSmoothTime);
+        m_rigidbody.AddForce(transform.forward * m_targetSpeed * Time.deltaTime * 50);
 
         m_animController.SetFloat("playerSpeed", Input.GetAxis("Horizontal") + Input.GetAxis("Vertical"));
     }
