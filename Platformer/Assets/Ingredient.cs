ea01bfeb2dadbc27af4cff667d0206b653ba0ead
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-03-19 23:09:26 +0100

    Implemented camera. Working in ingredients and in ingredientes spawners. Added some sprites.

---
+++ Platformer/Assets/Ingredient.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+
+    BoxCollider2D m_collider;
+    CircleCollider2D m_detectionArea;
+
+    void Awake()
+    {
+        m_collider = GetComponentInChildren<BoxCollider2D>();
+        m_detectionArea = GetComponentInChildren<CircleCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if(m_detectionArea.IsTouching(collision))
+        //{
+            collision.transform.position += Vector3.MoveTowards(transform.position, collision.transform.position, Time.deltaTime);
+        //}
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Platformer/Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    public int cameraExtraRelativeHeight = 1;
+
+    public float cameraSmoothTime = .1f;
+
+    public GameObject player;
+
+    Vector3 m_rotationSmoothVelocity;
+    Vector3 m_pos;
+
+    Rigidbody2D m_playerRigid;
+
+    void Awake()
+    {
+        m_pos = new Vector3();
+        m_playerRigid = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_pos.y = player.transform.position.y + cameraExtraRelativeHeight;
+        transform.position = Vector3.SmoothDamp(transform.position, m_pos, ref m_rotationSmoothVelocity, cameraSmoothTime); ;
+    }
+
+    void FixedUpdate()
+    {
+        //cameraExtraRelativeHeight = (m_playerRigid.velocity.y < 0) ? -1 : 1;
+    }
+}
