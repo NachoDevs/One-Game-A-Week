24acef713fac6fa887371d417194dfd4960410ab
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-03-26 22:49:12 +0100

    Player movemente Implemented. Working on controller support.

---
+++ Couch coop/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    GameManager m_gm;
+
+    void Awake()
+    {
+        m_gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    void Update()
+    {
+        if(m_gm.m_timePaused)
+        {
+            return;
+        }
+    }
+
+}
