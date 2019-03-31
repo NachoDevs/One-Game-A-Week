ebeea8ee55236105ff9e869f5d6e26cddf7724b4
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-03-31 11:41:11 +0200

    Flag implemented (end of the level)

---
+++ Couch coop/Assets/Scripts/Flag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    [SerializeField] BoxCollider2D m_poleCollider;
+    [SerializeField] CircleCollider2D m_topCollider;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponentInParent<Player>() != null)
+        {
+            //if (collision == m_poleCollider)
+            //{
+            //    print("top!!");
+            //}
+
+            Destroy(collision.transform.parent.gameObject);
+        }
+    }
+
+}
