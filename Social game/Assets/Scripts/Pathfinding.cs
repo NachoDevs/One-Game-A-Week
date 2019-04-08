e98f74beae441b73866f68a47f0091300fa6045b
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-04-08 22:30:44 +0200

    Player movement Implemented.

---
+++ Social game/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour
+{
+    public bool isPlayer = false;
+
+    public Camera cam;
+
+    //Vector3 m_targetPosition;
+
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(isPlayer)
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit))
+                {
+                    agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
