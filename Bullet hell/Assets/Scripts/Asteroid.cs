73c515a9297071d2c794b3c401669f67738589b7
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-04-22 22:10:43 +0200

    Bullet hell initial commit. Player movement Implemented, asteroids and player scripts started.

---
+++ Bullet hell/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public List<Sprite> sprites;
+
+    PolygonCollider2D m_collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AsteroidSetUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AsteroidSetUp()
+    {
+        GetComponentInChildren<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+        m_collider = GetComponentInChildren<Transform>().gameObject.AddComponent<PolygonCollider2D>();
+        m_collider.isTrigger = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collidedGO = collision.transform.parent.gameObject;
+
+        //if(collidedGO.GetComponent<Player>())
+        {
+
+        }
+
+    }
+}
---
+++ Bullet hell/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D m_rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementInput();
+
+        HandleLookDirection();
+    }
+
+    void HandleMovementInput()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.position -= new Vector3(x , y, 0) * .1f;
+    }
+
+    void HandleLookDirection()
+    {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
