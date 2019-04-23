1c232e2fe72356754a437a8908bd0c144a163b9e
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-04-23 22:24:16 +0200

    Player Projectiles Implemented.

--- Bullet hell/Assets/Scripts/Player.cs
+++ Bullet hell/Assets/Scripts/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float fireRate = 2.0f;
+
+    public GameObject projectile;
+
+    private float m_timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +19,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        m_timer += Time.deltaTime;
+
+        if (Input.GetAxis("Fire1") > 0)
+        {
+            if (m_timer >= fireRate)
+            {
+                m_timer = .0f;
+                Shoot();
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         Die();
+    }
+
+    void Shoot()
+    {
+        Vector3 instPos = transform.position + (transform.right * .6f);
+        GameObject proj = Instantiate(projectile, instPos, transform.rotation);
+        proj.GetComponent<Rigidbody2D>().AddForce(transform.right * 500);
     }
 
     void Die()
