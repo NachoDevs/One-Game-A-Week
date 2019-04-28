f4baae0e88b667c56630a0f398dbecceff856926
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-04-28 16:36:43 +0200

    Player movement echo Implemented.

---
+++ Bullet hell/Assets/Scripts/EchoEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoEffect : MonoBehaviour
+{
+    public float startTimeBetweenSpawns;
+
+    public GameObject echo;
+
+    float m_timeBetweenSpawns;
+
+    void Update()
+    {
+        if(m_timeBetweenSpawns <= 0)
+        {
+            GameObject go = Instantiate(echo, transform.position, transform.rotation);
+            Destroy(go, .5f);
+            m_timeBetweenSpawns = startTimeBetweenSpawns;
+        }
+        else
+        {
+            m_timeBetweenSpawns -= Time.deltaTime;
+        }
+    }
+
+}
