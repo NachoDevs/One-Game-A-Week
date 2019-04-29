2e03767db108372399d9f92c4e91feef29050875
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-04-29 19:54:28 +0200

    Initial commit. Main character animations, character basic movement Implemented.

---
+++ Fighting game/Assets/Scripts/BlockEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockEffect : MonoBehaviour
+{
+    Animator m_animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponentInChildren<Animator>();
+
+        m_animator.SetTrigger("block3");
+        //m_animator.SetTrigger("block" + Random.Range(0, 3));
+    }
+}
---
+++ Fighting game/Assets/Scripts/Player2DMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2DMovement : MonoBehaviour
+{
+
+    public float movementSpeed = 20f;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    bool m_isJumping;
+
+    float m_horizontalMove = 0f;
+
+    CharacterController2D m_controller;
+    Rigidbody2D m_rb;
+
+    GameManager m_gm;
+
+    void Awake()
+    {
+        m_gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        m_controller = GetComponent<CharacterController2D>();
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_gm.m_timePaused)
+        {
+            return;
+        }
+
+        m_horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_isJumping = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (m_gm.m_timePaused)
+        {
+            return;
+        }
+
+        m_controller.Move(m_horizontalMove * Time.fixedDeltaTime, false, m_isJumping);
+
+        m_isJumping = false;
+
+        if (m_rb.velocity.y < 0)
+        {
+            m_rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (m_rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            m_rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
