317f6b3cbd5eef4bada12a46d6ddb945e1d2a53f
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-05-05 17:03:28 +0200

    Deadzone trigger Implemented.

---
+++ Fighting game/Assets/Scripts/Deadzone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deadzone : MonoBehaviour
+{
+    Vector3 respawnPos = new Vector3(.0f, 2.5f, .0f);
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponentInParent<PlayerScript>() != null)
+        {
+            collision.transform.parent.transform.position = respawnPos;
+        }
+        else
+        {
+            Destroy(collision.transform.parent.transform);
+        }
+    }
+}
