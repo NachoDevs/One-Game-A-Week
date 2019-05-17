c6d5db070d9e9b2f51c5e843f660d9879a373175
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-05-17 21:39:02 +0200

    Shop items prefab created.

---
+++ Clicker/Assets/Scripts/ShopItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour
+{
+    [Header("UI")]
+    public Image itemImage;
+    public TextMeshProUGUI itemName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Clicker/Assets/Scripts/ShopMenu.cs
+++ Clicker/Assets/Scripts/ShopMenu.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopMenu : MonoBehaviour
 {
+    [Header("UI")]
+    public Image toggleButtonImage;
+
+    [Header("Items")]
+    public GameObject itemPrefab;
+    public Transform itemsParent;
+
+    [Space]
+
     [SerializeField]
     Vector3 m_hidePos = new Vector3(-250, 0, 0);
     [SerializeField]
@@ -19,7 +29,6 @@ public class ShopMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -45,5 +54,9 @@ public class ShopMenu : MonoBehaviour
         m_isVisible = !m_isVisible;
         m_position = (m_isVisible) ? m_visiblePos : m_hidePos;
         m_isMoving = true;
+
+        Vector3 theScale = toggleButtonImage.transform.localScale;
+        theScale.y *= -1;
+        toggleButtonImage.transform.localScale = theScale;
     }
 }
