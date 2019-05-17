262f6ca434524b2d31c7f023b850fda4a134fa55
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-05-17 20:29:33 +0200

    Main Mechanic Implemented.

---
+++ Clicker/Assets/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float currentPoints = 0;
+
+    [Header("UI")]
+    public TextMeshProUGUI pointsText;
+
+    Camera m_mainCamera1;
+    Clicker m_clicker;
+
+    RaycastHit2D m_hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_mainCamera1 = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointsText.text = ((int)currentPoints).ToString();
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            m_hit = Physics2D.Raycast(m_mainCamera1.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (m_hit.collider == null)
+            {
+                return;
+            }
+
+            m_clicker = m_hit.collider.GetComponentInParent<Clicker>();
+            if(m_clicker != null)
+            {
+                AddPoints(m_clicker.clickPoints);
+            }
+        }
+    }
+
+    public void AddPoints(float t_points)
+    {
+        currentPoints += t_points;
+    }
+}
---
+++ Clicker/Assets/Scripts/Clicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    public float clickPoints = 1;
+
+    float pointsPerFrame = 0;
+
+    GameManager m_gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_gm.AddPoints(pointsPerFrame);
+    }
+
+}
