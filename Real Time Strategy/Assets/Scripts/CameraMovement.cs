251b2034a9673f525f91498d4ccabe7f9ec4ff6a
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-05-28 07:40:05 +0200

    Camera movement Implemented.

---
+++ Real Time Strategy/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    float cameraMovSpeed = 50f;
+    [SerializeField]
+    float cameraMaxMoveSpeed = 1;
+
+    Vector3 newCameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newCameraPos = Vector3.zero;
+
+        newCameraPos.x += Input.GetAxisRaw("Vertical");
+        newCameraPos.z += Input.GetAxisRaw("Vertical");
+
+        newCameraPos.x += Input.GetAxisRaw("Horizontal");
+        newCameraPos.z -= Input.GetAxisRaw("Horizontal");
+
+        newCameraPos.x = Mathf.Clamp(newCameraPos.x, -cameraMaxMoveSpeed, cameraMaxMoveSpeed);
+        newCameraPos.z = Mathf.Clamp(newCameraPos.z, -cameraMaxMoveSpeed, cameraMaxMoveSpeed);
+
+        transform.position += newCameraPos * Time.deltaTime * cameraMovSpeed;
+    }
+}
