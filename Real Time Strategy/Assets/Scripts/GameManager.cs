a15bb4e2cf211c54ab06c5ed0e0083bab4196771
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-05-28 08:35:40 +0200

    Team setup and building instantiating Implemented.

---
+++ Real Time Strategy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> buildings;
+
+    public List<Material> teamMats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject building = Instantiate(buildings[0]);
+        building.GetComponent<Building>().team = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum BuildingType
+{
+    HQ,
+}
