8b15b88d38897d3384722b37188d6c753a8061f3
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-06-05 17:40:49 +0200

    Improving tile selection and behaviour for the Input.

--- Town building game/Assets/Scripts/GameManager.cs
+++ Town building game/Assets/Scripts/GameManager.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public enum GameState
 {
@@ -12,11 +9,20 @@ public enum GameState
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Prefabs")]
+    public GameObject buildingPrefab;
+
+    [Header("Parents")]
+    public Transform buildingParent;
+
     GameState m_currGameState;
 
     RaycastHit2D m_hit;
 
     Camera m_cam;
+
+    GameObject m_selectedObject;
+    GameObject m_hoveredObject;
 
     TileManager m_tman;
 
@@ -47,49 +53,93 @@ public class GameManager : MonoBehaviour
     {
         switch(m_currGameState)
         {
+            case GameState.Building:
+                TileSelectionBehaviour();
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    GameObject building = Instantiate(buildingPrefab, m_currentTile.transform.position, Quaternion.identity, buildingParent);
+                }
+
+                break;
             default:
             case GameState.TileSelecting:
-                try
+                TileSelectionBehaviour();
+                break;
+        }
+    }
+
+    void TileSelectionBehaviour()
+    {
+        try
+        {
+            m_hit = Physics2D.Raycast(m_cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            m_hoveredObject = m_hit.collider.transform.parent.gameObject;
+
+            m_previousTile = m_currentTile;
+
+            m_currentTile = m_tman.GetTile(m_hoveredObject.transform.position.x, m_hoveredObject.transform.position.y);
+
+            if (!m_currentTile.selected)
+            {
+                m_currentTile.UpdateTIle(TileState.Hovered);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                m_selectedObject = m_hoveredObject;
+
+                HandleSelection();
+            }
+
+            if (!m_previousTile.selected)
+            {
+                if (m_previousTile != m_currentTile)
                 {
-                    m_hit = Physics2D.Raycast(m_cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-
-                    m_previousTile = m_currentTile;
-
-                    m_currentTile = m_tman.GetTile(m_hit.transform.position.x, m_hit.transform.position.y);
-
-                    if(!m_currentTile.selected)
-                    {
-                        m_currentTile.UpdateTIle(TileState.Hovered);
-                    }
-                    //m_currentTile.hovered = true;
-
-                    if (Input.GetMouseButtonUp(0))
-                    {
-                        m_currentTile.UpdateTIle(TileState.Selected);
-
-                        // Only one tile can be selected
-                        foreach (GameObject wt in m_tman.unsortedNodes)
-                        {
-                            if(wt.GetComponent<WorldTile>() != m_currentTile)
-                            {
-                                wt.GetComponent<WorldTile>().UpdateTIle(TileState.Default);
-                            }
-                        }
-                    }
-
-                    if(!m_previousTile.selected)
-                    {
-                        if(m_previousTile != m_currentTile)
-                        {
-                            m_previousTile.UpdateTIle(TileState.Default);
-                        }
-                        //m_currentTile.hovered = false;
-                    }
-
+                    m_previousTile.UpdateTIle(TileState.Default);
                 }
-                catch (Exception e) { PrintException(e); }
-            break;
+            }
+        }
+        catch (Exception e) { /*PrintException(e);*/ }
+    }
 
+    void HandleSelection()
+    {
+        if(m_selectedObject.GetComponent<WorldTile>() != null)
+        {
+            m_currentTile.UpdateTIle(TileState.Selected);
+
+            // Only one tile can be selected
+            foreach (GameObject wt in m_tman.unsortedNodes)
+            {
+                if (wt.GetComponent<WorldTile>() != m_currentTile)
+                {
+                    wt.GetComponent<WorldTile>().UpdateTIle(TileState.Default);
+                }
+            }
+
+            return;
+        }
+
+        if (m_selectedObject.GetComponent<Building>() != null)
+        {
+            print("this is a building");
+
+            return;
+        }
+    }
+
+    public void GoToBuildingMode()
+    {
+        switch(m_currGameState)
+        {
+            default:
+            case GameState.TileSelecting:
+                m_currGameState = GameState.Building;
+                break;
+            case GameState.Building:
+                m_currGameState = GameState.TileSelecting;
+                break;
         }
     }
 
