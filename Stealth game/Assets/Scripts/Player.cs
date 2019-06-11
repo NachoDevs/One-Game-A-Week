3932af68ecbb1b6d59e00f3f7f78a4e88de8b888
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-06-11 16:54:04 +0200

    Basic movement Implemented.

---
+++ Stealth game/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(movementSpeed * Time.deltaTime,
+                                    0,
+                                    movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-movementSpeed * Time.deltaTime,
+                                                0,
+                                                movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(-movementSpeed * Time.deltaTime,
+                                                0,
+                                                -movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(movementSpeed * Time.deltaTime,
+                                0,
+                                -movementSpeed * Time.deltaTime);
+        }
+
+        // Boundaries, to be improved
+        if (transform.position.x <= -65)
+        {
+            transform.position = new Vector3(-60, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x >= 35)
+        {
+            transform.position = new Vector3(30, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z <= -35)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -30);
+        }
+        if (transform.position.z >= 20)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 16);
+        }
+    }
+}
